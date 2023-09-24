79348c36db9d5252c907996ec52ea91c0909a71c
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-09-24 13:21:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+let arr =  ['Tajmahal',
+			'Victoria Memorial',
+			'The Virupaksha Temple'
+		   ];
+let arrless =[];
+let mp = {};
+
+
+for(let i=0; i<arr.length; i++)
+{
+
+	let articlelessitem = arr[i].replace(regex,"").trim();
+	arrless.push(articlelessitem);
+	mp[articlelessitem] = arr[i];
+}
+arrless.sort();
+let ans = []
+
+for(let i of arrless)
+	{
+		ans.push(mp[i]);
+	}
+
+console.log(ans);
+
 
